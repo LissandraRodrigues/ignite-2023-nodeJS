6da811717fb214c13aed2e3a6a37ec3e82d05734
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-10 18:45:24 -0300

    feature: Add JSON middleware

--- 01-nodeJS-fundamentals/src/server.js
+++ 01-nodeJS-fundamentals/src/server.js
@@ -1,32 +1,33 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const port = 3333
 
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
 
     const { method, url } = request
 
+    await json(request, response)
+
     if (method === 'GET' && url === '/users') {
-
-        return response
-            .setHeader('Content-type', 'application/json')
-            .end(JSON.stringify(users))
-
+        return response.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
 
+        const { name, email } = request.body
+
         const user = {
             id: 1,
-            name: 'John Doe',
-            email: 'john@gmail.com'
+            name,
+            email
         }
 
         users.push(user)
 
-        return response.writeHead(201).end('Criação de usuário')
+        return response.writeHead(201).end('User created.')
 
     }
 
