c0f5b2293a515a0cc70cf00ae587ccaea9ee6c66
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-10 17:07:35 -0300

    feature: Add stream server

---
+++ 01-nodeJS-fundamentals/streams/stream-http-server.js
@@ -0,0 +1,30 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+const port = 3334
+
+class InverseNumberStream extends Transform {
+
+    _transform(chunk, enconding, callback) {
+
+       const transformed = Number(chunk.toString()) * -1
+       
+       console.log(transformed)
+       
+       callback(null, Buffer.from(String(transformed)))
+
+    }
+
+}
+
+const server = http.createServer((request, response) => {
+
+    return request
+        .pipe(new InverseNumberStream())
+        .pipe(response)
+
+})
+
+server.listen(port, (() => {
+    console.log('Running on port: ', port)
+}))
