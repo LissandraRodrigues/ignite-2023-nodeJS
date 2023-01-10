91d9caae7e70be321f8fc6cf7c3d0e222f821ac5
LuizaLissandra <luizalissandrarosa@poli.ufrj.br>
2023-01-10 14:44:27 -0300

    feature: Add initial GET and POST route structure

--- 01-nodeJS-fundamentals/src/server.js
+++ 01-nodeJS-fundamentals/src/server.js
@@ -2,12 +2,38 @@ import http from 'node:http'
 
 const port = 3333
 
+const users = []
+
 const server = http.createServer((request, response) => {
 
-    return response.end('Hello')
+    const { method, url } = request
+
+    if (method === 'GET' && url === '/users') {
+
+        return response
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+
+    }
+
+    if (method === 'POST' && url === '/users') {
+
+        const user = {
+            id: 1,
+            name: 'John Doe',
+            email: 'john@gmail.com'
+        }
+
+        users.push(user)
+
+        return response.writeHead(201).end('Criação de usuário')
+
+    }
+
+    return response.writeHead(404).end('Not found.')
 
 })
 
 server.listen(port, (() => {
-    console.log("Running on port: ", port)
+    console.log('Running on port: ', port)
 }))
